2bf8cb8f28a1bfe4a82fd3cdf40610bb575dad01
anuragkumar96 <s531372@nwmissouri.edu>
2020-01-07 20:40:07 -0600

    Implemenation of hard coded Authentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -9,13 +9,23 @@ export class LoginComponent implements OnInit {
 
   username='anurag'
   password=''
+  errorMessage='invalid credentials'
+  invalidLogin= false
   constructor() { }
 
   ngOnInit() {
   }
   handleLogin(){
+
+    if(this.username==='anurag' && this.password==='hello')
+    {
+    this.invalidLogin=false
     console.log(this.username)
     console.log(this.password)
+    }
+    else{
+      this.invalidLogin=true
+    }
   }
 
 }
